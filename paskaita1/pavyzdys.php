be8bb95cc960b226330ad060d37f8c22ee44d00a
unknown <gintarasx@gmail.com>
2017-11-15 13:12:05 +0200

    aaa

---
+++ paskaita1/pavyzdys.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h1>Labas, <?php
+            $vardas = "Jonas";
+            echo $vardas; ?></h1>
+    <footer>&copy; <?php echo date("Y"); ?></footer>
+    </body>
+</html>
